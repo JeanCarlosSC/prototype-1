afbc39fd193d18eedaefa1c482e84d34ed11528d
JeanCarlosSC <jeancarlosodi@gmail.com>
2022-12-12 12:28:32 -0500

    smothing the camera with LateUpdate and updating its position

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,7 +6,7 @@ public class FollowPlayer : MonoBehaviour {
 
     public GameObject player;
 
-    private Vector3 offset = new Vector3(0, 6, -8);
+    private Vector3 offset = new Vector3(0, 6, -9);
 
     // Start is called before the first frame update
     void Start() {
@@ -14,7 +14,7 @@ public class FollowPlayer : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update() {
+    void LateUpdate() {
         // Offset the camera behind the player by adding to the player's position
         transform.position = player.transform.position + offset;
     }
